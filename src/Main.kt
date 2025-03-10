7e667265ed0ccdb62d1421a420e5cb268150710d
Cavin <cavinlarry001@gmail.com>
2025-03-10 12:54:04 +0300

    Implemented the second task temperature converter on temperatureConverter.kt check it out

--- src/Main.kt
+++ src/Main.kt
@@ -1,3 +1,19 @@
 fun main(){
     println("Hello there, thanks for checking the kotlin-intro-projects, happy hacking and coding!")
 }
+
+/*
+fun passwordChecker(password: String?) {
+    val myPassword = if (password.isNullOrEmpty() || password.length < 6) {
+        "default12345"
+    } else {
+        password
+    }
+
+    println("Your password is: $myPassword")
+}
+
+fun main() {
+    val pass = readLine()
+    passwordChecker(pass)
+}*/
