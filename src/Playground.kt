75aad6745ca91d549edd2f5bfd1c9fac7cefb1af
Cavin <cavinlarry001@gmail.com>
2025-04-30 22:38:51 +0300

    chore: Update day 8(Array.kt), solved day 9 (DictMap.kt) on Hackerrank

--- src/Playground.kt
+++ src/Playground.kt
@@ -1,1 +1,12 @@
-fun isNumber(input: String) : Any = input.toIntOrNull()?: input // write this function
+fun main() {
+    val myArray = arrayOf(1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
+    println(
+        try {
+            myArray[12]
+        } catch (e: ArrayIndexOutOfBoundsException) {
+            "No element found at that index, probably it's out of bound! ${e.message}"
+        } finally {
+            "Exiting..."
+        }
+    )
+}
--- src/hackerrank/Arrays.kt
+++ src/hackerrank/Arrays.kt
@@ -1,10 +1,9 @@
 package hackerrank
 
+fun main() {
+    val n = readLine()!!.trim().toInt()
 
-fun main(args: Array<String>) {
-    val n = readln().trim().toInt()
-
-    val arr: Array<Int> = readln().trimEnd().split(" ").map{ it.toInt() }.toTypedArray()
-
-    println(arr.joinToString(" ").reversed())
+    val arr = readLine()!!.trimEnd().split(" ").map{ it.toInt() }.toTypedArray()
+    require(arr.size == n)
+    println(arr.reversed().joinToString(" "))
 }
