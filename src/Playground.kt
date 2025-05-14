9d34a566409db6855ea0283d0dd6615605861fe8
Cavin <cavinlarry001@gmail.com>
2025-05-14 20:02:38 +0300

    Update

--- src/Playground.kt
+++ src/Playground.kt
@@ -1,12 +1,12 @@
+import kotlin.random.Random
+
+fun generateTemperature(seed: Int): String{
+    // write your code here
+    val random = Random(seed)
+    return (1..7).map { random.nextInt(20, 30) }.joinToString(" ")
+}
+
+
 fun main() {
-    val myArray = arrayOf(1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
-    println(
-        try {
-            myArray[12]
-        } catch (e: ArrayIndexOutOfBoundsException) {
-            "No element found at that index, probably it's out of bound! ${e.message}"
-        } finally {
-            "Exiting..."
-        }
-    )
+    print(generateTemperature(1))
 }
